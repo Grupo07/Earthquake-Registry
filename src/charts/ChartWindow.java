209cfbb28326e2014851535e53c362862540f891
Lumanter <luismariano10del4@gmail.com>
2020-05-12 14:23:42 -0600

    Added chart window class

---
+++ src/charts/ChartWindow.java
@@ -0,0 +1,47 @@
+
+package charts;
+
+
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+
+public class ChartWindow extends JFrame{
+    
+    private ArrayList<Earthquake> earthquakes;
+    
+    public ChartWindow(ArrayList<Earthquake> earthquakes) {
+        super("Graficos de Sismos");
+        this.setSize(800, 500);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.earthquakes = earthquakes;
+    }
+    
+    public void showEarthquakesByFaultOrigin() {
+        JPanel chartPanel = new PieChart(this.earthquakes).getPanel();
+        this.setContentPane(chartPanel);
+    }
+    
+    public void showEarthquakesPerMonth(int year) {
+        JPanel chartPanel = new BarChart(this.earthquakes, year).getPanel();
+        this.setContentPane(chartPanel);
+    }
+    
+    public void showEarthquakesByMagnitudeRanges() {
+        JPanel chartPanel = new HistogramChart(this.earthquakes).getPanel();
+        this.setContentPane(chartPanel);
+    }
+    
+    public void showEarthquakesByMagnitudeRanges(Province province) {
+        JPanel chartPanel = new HistogramChart(this.earthquakes, province).getPanel();
+        this.setContentPane(chartPanel);
+    }
+    
+    public void display() {
+        this.setVisible(true);
+    }
+    
+}
